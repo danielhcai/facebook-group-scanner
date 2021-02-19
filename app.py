48a0c62d51a9c4a0fff4faa9d93fc9cdcefa8a8b
Daniel Cai <danielhcai@gmail.com>
2021-02-19 00:08:33 -0600

    Better scanner

--- app.py
+++ app.py
@@ -6,51 +6,37 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 
-NUM_SCROLLS = 5
-SCROLL_PAUSE_TIME = 3
+EMAIL_REG_EX = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"
 
 if __name__ == "__main__":
 	# Command line args
 	import argparse
 	parser = argparse.ArgumentParser()
-	parser.add_argument("-u", "--username", type=str, default="")
-	parser.add_argument("-p", "--password", type=str, default="")
-	parser.add_argument("-g", "--group", type=str, default="")
 	parser.add_argument("-f", "--file", type=str, default="temp")
 	args = parser.parse_args()
 
-	# Check for no username or password
-	if args.username == "" or args.password == "" or args.group == "":
-		print("Bad arguemnts")
-	else:
-		# Open browser and go to facebook
-		driver = webdriver.Firefox(executable_path=r".\geckodriver.exe")
-		driver.get("https://www.facebook.com/")
+	# Open browser and go to facebook
+	driver = webdriver.Firefox(executable_path=r".\geckodriver.exe")
+	driver.get("https://www.facebook.com/")
 
-		# Login to facebook and go to facebook group
-		username_input = driver.find_element_by_id("email")
-		username_input.send_keys(args.username)
-		password_input = driver.find_element_by_id("pass")
-		password_input.send_keys(args.password)
-		login_button = driver.find_element_by_id("u_0_b")
-		login_button.click()
-		driver.get(args.group)
+	input("Press enter to continue...")
 
-		input("Press enter to continue...")
+	# Get posts and convert to beautiful soup
+	feed = driver.find_element_by_xpath("//html")
+	soup = BeautifulSoup(feed.get_attribute("innerHTML"), 'html.parser')
+	text = soup.get_text(separator=" ", strip=True)
 
-		# Get posts and convert to beautiful soup
-		feed = driver.find_element_by_xpath("//html")
-		soup = BeautifulSoup(feed.get_attribute("innerHTML"), 'html.parser')
-		text = soup.get_text(separator=" ", strip=True)
+	# "[a-zA-Z0-9.]+[@][a-zA-Z0-9]+[.][a-zA-Z]+"
+	emails = re.findall("[a-zA-Z0-9.]+[@][a-zA-Z0-9]+[.][a-zA-Z]+", text)
+	unique_emails = set()
 
-		# "[(]*([0-9]){3}[-) ]*([0-9]){3}[- ]*([0-9]){4}"
-		emails = re.findall("[a-zA-Z0-9.]+[@][a-zA-Z0-9]+[.][a-zA-Z]+", text)
-		unique_emails = set(emails)
-
-		file = open("emails/" + args.file + ".txt", "w")
-		for email in unique_emails:
+	file = open("emails/" + args.file + ".txt", "w")
+	for email in emails:
+		if email not in unique_emails:
 			file.write(email + "\n")
-		file.close()
+			unique_emails.add(email)
 
-		# Close browser
-		driver.close()
+	file.close()
+
+	# Close browser
+	driver.close()
